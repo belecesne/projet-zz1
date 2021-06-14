6ef6403d0e4811ef74339c33e852f67aa4f1e1c0
Loïc VIMIEN <loic.vimien@etu.uca.fr>
2021-06-14 10:33:15 +0200

    X_fenêtré fini

--- exos/x_fenetre/graphics.c
+++ exos/x_fenetre/graphics.c
@@ -18,8 +18,40 @@ SDL_Window * createWindow(int xPos, int yPos, int width, int height) {
 }
 
 SDL_Window *** createWindowMatrix(int length) {
-    SDL_Window *** matrix = calloc(length, sizeof(SDL_Window***));
+    SDL_Window *** matrix = (SDL_Window***) calloc(length, sizeof(SDL_Window***));
     for(int i = 0; i < length; i++) {
-        
+        matrix[i] = (SDL_Window**) calloc(length, sizeof(SDL_Window**));
     }
+    return matrix;
+}
+
+void initMatrix(SDL_Window *** matrix, int length, int screen_width, int screen_height) {
+    int curr_x = 0;
+    int curr_y = 0;
+    const int window_x_size = screen_width / length;
+    const int window_y_size = screen_height / length;
+    for(int i = 0; i < length; i++) {
+        matrix[i][i] = createWindow(curr_x, curr_y, window_x_size, window_y_size);
+        curr_x += window_x_size;
+        curr_y += window_y_size;
+    }
+    curr_x = screen_width - window_x_size;
+    curr_y = 0;
+    for(int i = 0; i < length; i++) {
+        matrix[i][length-i] = createWindow(curr_x, curr_y, window_x_size, window_y_size);
+        curr_x -= window_x_size;
+        curr_y += window_y_size;
+    }
+}
+
+void destroyMatrix(SDL_Window*** matrix, int length) {
+    for(int i = 0; i < length; i++) {
+        for(int j = 0; j < length; j++) {
+            if(matrix[i][j] != NULL) {
+                SDL_DestroyWindow(matrix[i][j]);
+            }
+        }
+        free(matrix[i]);
+    }
+    free(matrix);
 }
