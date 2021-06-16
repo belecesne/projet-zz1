2f0deb07ba29b1ec489f3244e91b6dac9d79514d
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-16 19:25:15 +0200

    feature: Animation gauche et droite en course

--- exos/game/headers/animation.h
+++ exos/game/headers/animation.h
@@ -7,7 +7,7 @@
 #include "player.h"
 
 void drawAnimationLoop(SDL_Rect *, int, SDL_Texture *, SDL_Texture *, int, SDL_Window *, SDL_Renderer *);
-void drawOneFrame(SDL_Rect * , int , SDL_Texture * , SDL_Window * , SDL_Renderer * , int * , player_t *);
+void drawOneFrame(SDL_Rect * , int , SDL_Texture * , SDL_Window * , SDL_Renderer * , int  , player_t *, int);
 SDL_Rect * loadAnimationPos(int , int , int , int );
 
 #endif
--- exos/game/src/animation.c
+++ exos/game/src/animation.c
@@ -1,36 +1,43 @@
- #include "../headers/animation.h"
- #include "../headers/graphics.h"
- #include <string.h>
+#include "../headers/animation.h"
+#include "../headers/graphics.h"
+#include <string.h>
 
-void drawAnimationLoop(SDL_Rect * frames, int nb_frames, SDL_Texture * texture, SDL_Texture * background, int frameRate, SDL_Window * window, SDL_Renderer * renderer) {
+void drawAnimationLoop(SDL_Rect *frames, int nb_frames, SDL_Texture *texture, SDL_Texture *background, int frameRate,
+                       SDL_Window *window, SDL_Renderer *renderer) {
     SDL_Rect source = {0}, dest = {0}, sourceBg = {0}, destBg = {0};
     dest.w = 100;
     dest.h = 100;
     SDL_QueryTexture(background, NULL, NULL, &sourceBg.w, &sourceBg.h);
     SDL_GetWindowSize(window, &destBg.w, &destBg.h);
-    for(int i = 0; i < nb_frames; i++) {
+    for (int i = 0; i < nb_frames; i++) {
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, background, &sourceBg, &destBg);
         SDL_RenderCopy(renderer, texture, frames + i, &dest);
         SDL_RenderPresent(renderer);
-        SDL_Delay(1000/frameRate);
+        SDL_Delay(1000 / frameRate);
     }
 }
 
-void drawOneFrame(SDL_Rect * frames, int nb_frames, SDL_Texture * texture, SDL_Window * window, SDL_Renderer * renderer, int * currentFrame, player_t * player) {
+void drawOneFrame(SDL_Rect *frames, int nb_frames, SDL_Texture *texture, SDL_Window *window, SDL_Renderer *renderer,
+                  int currentFrame, player_t *player, int flipped) {
     SDL_Rect source = {0}, dest = {0};
-dest.x = player->rect.x;
-dest.y = player->rect.y;
+    dest.x = player->rect.x;
+    dest.y = player->rect.y;
     dest.w = 100;
     dest.h = 100;
-    SDL_RenderCopy(renderer, texture, frames + *currentFrame, &dest);
-	*currentFrame = (*currentFrame + 1) % nb_frames;
+    SDL_Point center = {dest.x + dest.w / 2, dest.y + dest.h / 2};
+
+    if (!flipped) {
+        SDL_RenderCopy(renderer, texture, frames + currentFrame, &dest);
+    } else {
+        SDL_RenderCopyEx(renderer, texture, frames + currentFrame, &dest, 0, &center, SDL_FLIP_HORIZONTAL);
+    }
+
 }
 
-SDL_Rect * loadAnimationPos(int line, int numberColumns, int lineHeight, int columnWidth) {
-    SDL_Rect * frames = malloc(numberColumns * sizeof(SDL_Rect));
-    int yCurr = line*lineHeight, xCurr = 0;
-    for(int i = 0; i < numberColumns; i++) {
+SDL_Rect *loadAnimationPos(int line, int numberColumns, int lineHeight, int columnWidth) {
+    SDL_Rect *frames = malloc(numberColumns * sizeof(SDL_Rect));
+    int yCurr = line * lineHeight, xCurr = 0;
+    for (int i = 0; i < numberColumns; i++) {
         frames[i].x = xCurr;
         frames[i].y = yCurr;
         frames[i].w = columnWidth;
