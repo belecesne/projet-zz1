b7158075c02290c0fb9b7adcd729a002eecb85ec
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-16 09:54:17 +0200

    feat: Fermeture de la fenêtre avec SDL_Quit

--- exos/game/src/main.c
+++ exos/game/src/main.c
@@ -1,16 +1,36 @@
 #include "../headers/main.h"
 
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     srand(time(NULL));
 
     SDL_DisplayMode disp;
-    SDL_Window * window;
+    SDL_Window *window;
     SDL_Renderer *renderer;
     initGraphics();
     SDL_GetCurrentDisplayMode(0, &disp);
     window = createWindow(10, 10, WINDOW_W, WINDOW_H);
     renderer = createRenderer(window);
-    SDL_Delay(3000);
+
+    SDL_bool program_on = SDL_TRUE;
+    while (program_on) {
+        SDL_Event event;
+        while (program_on && SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT :
+                    program_on = SDL_FALSE;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        default:
+                            break;
+                    }
+                default:
+                    break;
+            }
+        }
+        SDL_Delay(10);
+
+    }
     return 0;
 }
