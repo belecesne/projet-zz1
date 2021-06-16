6b76a93a5b957e2746eea496d7dc683e51181a76
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-16 09:31:03 +0200

    refactor: Création des différents dossiers du chef d'oeuvre

---
+++ exos/game/headers/main.h
@@ -0,0 +1,4 @@
+#ifndef GAME_ZZ1_MAIN_H
+#define GAME_ZZ1_MAIN_H
+
+#endif //GAME_ZZ1_MAIN_H
---
+++ exos/game/src/main.c
@@ -0,0 +1,7 @@
+#include "../headers/main.h"
+
+
+int main(int argc, char * argc[]) {
+
+    return 0;
+}
