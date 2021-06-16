8dfe4b3e5fca2ef32592166dc3029e422d0e522d
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-16 09:38:20 +0200

    feat: Création de la fenêtre

--- exos/game/headers/main.h
+++ exos/game/headers/main.h
@@ -1,4 +1,12 @@
 #ifndef GAME_ZZ1_MAIN_H
 #define GAME_ZZ1_MAIN_H
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define WINDOW_H 800
+#define WINDOW_W 450
 #endif //GAME_ZZ1_MAIN_H
--- exos/game/src/main.c
+++ exos/game/src/main.c
@@ -2,6 +2,15 @@
 
 
 int main(int argc, char * argc[]) {
+    srand(time(NULL));
 
+    SDL_DisplayMode disp;
+    SDL_Window * window;
+    SDL_Renderer *renderer;
+    initGraphics();
+    SDL_GetCurrentDisplayMode(0, &disp);
+    window = createWindow(10, 10, WINDOW_W, WINDOW_H);
+    renderer = createRenderer(windows);
+    SDL_Delay(3000);
     return 0;
 }
