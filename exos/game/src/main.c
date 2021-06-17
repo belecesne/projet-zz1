5e4e650c33a2be4f55bcadd4768cd63295c83d36
Benjamin <benjamin.lecesne.blc@gmail.com>
2021-06-17 09:20:51 +0200

    fix: nettoyage du code

--- exos/game/src/main.c
+++ exos/game/src/main.c
@@ -40,7 +40,7 @@ int main(int argc, char* argv[]){
 	SDL_GetWindowSize(window, &destBg.w, &destBg.h);
 
 	int i;
-	int currentFrameRun = 0, currentFrameIdle = 0, currentFrameJump = 0, flipped = 0, win = 1, jumpDelay = 0, repeat = 0, score = 0;
+	int currentFrameRun = 0, currentFrameIdle = 0, currentFrameJump = 0, flipped = 0, win = 1, jumpDelay = 0, score = 0;
 
 	SDL_Point coordArray[8] = {{0,   100},
 	                           {150, 200},
