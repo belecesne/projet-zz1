78f8f19b09b3579c3ac8f0456b4cdd7da4df78f4
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-17 16:35:36 +0200

    feat: Partition opérationnelle. Opération en O(n*log(n))

---
+++ exos/partition/headers/union_find.h
@@ -0,0 +1,24 @@
+#ifndef UNION_FIND_ADRIEN_ZZ1_UNION_FIND_H
+#define UNION_FIND_ADRIEN_ZZ1_UNION_FIND_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEFAULT_UNION_FIND_SIZE 11
+
+typedef struct union_find_t{
+    int * partition;
+    int * level;
+    int size;
+}union_find_t;
+
+
+union_find_t * createUnionFind(int, int *);
+void freeUnionFind(union_find_t *);
+void initUnionFind(union_find_t *);
+void printUnionFind(union_find_t *);
+int rootNodeUnionFind(union_find_t *, int);
+void fusionUnionFind(union_find_t * unionFind, int, int);
+void printClassUnionFind(union_find_t *, int);
+void printAllClassesUnionFind(union_find_t *);
+#endif //UNION_FIND_ADRIEN_ZZ1_UNION_FIND_H
---
+++ exos/partition/src/main.c
@@ -0,0 +1,47 @@
+#include "../headers/union_find.h"
+
+
+int main() {
+    int erreur = 0;
+    union_find_t *unionFind;
+    unionFind = createUnionFind(DEFAULT_UNION_FIND_SIZE, &erreur);
+    if (!erreur) {
+        initUnionFind(unionFind);
+        printUnionFind(unionFind);
+
+
+        fusionUnionFind(unionFind, 0, 1);
+        printUnionFind(unionFind);
+
+        fusionUnionFind(unionFind, 2, 3);
+        printUnionFind(unionFind);
+
+        fusionUnionFind(unionFind, 10, 3);
+        printUnionFind(unionFind);
+
+        fusionUnionFind(unionFind, 5, 9);
+        printUnionFind(unionFind);
+
+        fusionUnionFind(unionFind, 4, 6);
+        printUnionFind(unionFind);
+
+
+        fusionUnionFind(unionFind, 8, 7);
+        printUnionFind(unionFind);
+
+
+        fusionUnionFind(unionFind, 7, 9);
+        printUnionFind(unionFind);
+
+
+        fusionUnionFind(unionFind, 6, 8);
+        printUnionFind(unionFind);
+
+        printClassUnionFind(unionFind,3);
+        printAllClassesUnionFind(unionFind);
+        freeUnionFind(unionFind);
+    }
+
+    return 0;
+}
+
