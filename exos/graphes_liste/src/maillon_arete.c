69ed852b2d085f2487d9bf5829beba9f77b11228
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-21 11:51:29 +0200

    feat: Fonctions de bases de la structure de liste d'aretes

--- exos/graphes_liste/src/maillon_arete.c
+++ exos/graphes_liste/src/maillon_arete.c
@@ -1,5 +1,29 @@
-//
-// Created by Adrien on 21/06/2021.
-//
-
 #include "../headers/maillon_arete.h"
+
+void insertionTeteClasse(maillon_arete_t **tete, maillon_arete_t *maillon) {
+    if (tete != NULL) {
+        maillon->suivant = *tete;
+        *tete = maillon;
+    }
+}
+
+maillon_arete_t *creerMaillon(maillon_arete_t **arete) {
+    maillon_arete_t *maillon;
+    maillon = malloc(sizeof(maillon_arete_t));
+    if (maillon != NULL) {
+        maillon->suivant = NULL;
+        maillon->arete = arete;
+    }
+    return maillon;
+}
+
+
+void libererListeClasses(maillon_arete_t *tete) {
+    maillon_arete_t *courant, *temp;
+    courant = tete;
+    while (courant) {
+        temp = courant->suivant;
+        free(courant);
+        courant = temp;
+    }
+}
