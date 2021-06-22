59de09bbc4d707adfb77ff09ab70fee4a44fe12b
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-22 13:12:53 +0200

    refactor: Revert des partitions + modification du type dans le vecteur arete

--- exos/graphes_liste/src/vecteur_arete.c
+++ exos/graphes_liste/src/vecteur_arete.c
@@ -1,26 +1,26 @@
 #include "../headers/vecteur_arete.h"
 
 
-vecteur_t * creerVecteurArete(int taille){
-    vecteur_t * liste = malloc(sizeof(vecteur_t));
+vecteur_arete_t * creerVecteurArete(int taille){
+    vecteur_arete_t * liste = malloc(sizeof(vecteur_arete_t));
     if(liste){
-        liste->array = malloc(taille*sizeof(vec_type));
+        liste->array = malloc(taille*sizeof(vec_arete_type));
         liste->tailleCourante = 0;
         liste->taille = taille;
     }
     return liste;
 }
 
-void reallouerVecteurArete(vecteur_t * liste){
+void reallouerVecteurArete(vecteur_arete_t * liste){
     int nouvelleTaille = liste->tailleCourante * 2;
-    vec_type * nouveauTab = malloc(nouvelleTaille * sizeof(vec_type));
-    memcpy(nouveauTab, liste->array, (liste->tailleCourante) * sizeof(vec_type));
+    vecteur_arete_t * nouveauTab = malloc(nouvelleTaille * sizeof(vec_arete_type));
+    memcpy(nouveauTab, liste->array, (liste->tailleCourante) * sizeof(vec_arete_type));
     free(liste->array);
     liste->array = nouveauTab;
     liste->taille *= 2;
 }
 
-void insererQueueVecteurArete(vecteur_t * liste, vec_type inser){
+void insererQueueVecteurArete(vecteur_arete_t * liste, vec_arete_type inser){
         if(liste->tailleCourante == liste->taille){
             reallouerVecteurArete(liste);
         }
@@ -28,13 +28,13 @@ void insererQueueVecteurArete(vecteur_t * liste, vec_type inser){
         liste->tailleCourante++;
 }
 
-void supprimerVecteurArete(vecteur_t ** liste, int index){
+void supprimerVecteurArete(vecteur_arete_t ** liste, int index){
     free((*liste)->array[index]);
     (*liste)->array[index] =(*liste)->array[(*liste)->tailleCourante-1];
     (*liste)->tailleCourante--;
 }
 
-void libererVecteurArete(vecteur_t * liste){
+void libererVecteurArete(vecteur_arete_t * liste){
     free(liste->array);
     free(liste);
 }
