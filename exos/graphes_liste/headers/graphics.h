982ff45aada2c48b2585a317c24626bf309f8d0e
Adrien <adrien.casteleiro@etu.uca.fr>
2021-06-22 09:05:12 +0200

    feat: Ajout des déclarations et initialisations des variables de la SDL

--- exos/graphes_liste/headers/graphics.h
+++ exos/graphes_liste/headers/graphics.h
@@ -4,6 +4,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#define WINDOW_W 1000
+#define WINDOW_H 1000
+
 void initGraphics();
 SDL_Window * createWindow(int, int, int, int);
 SDL_Renderer * createRenderer(SDL_Window *);
--- exos/graphes_liste/src/main.c
+++ exos/graphes_liste/src/main.c
@@ -1,12 +1,21 @@
 #include "../headers/graph.h"
 #include "../headers/maillon_arete.h"
 #include "../headers/labyrinthe.h"
+#include "../headers/graphics.h"
 #include <time.h>
 #define LIGNE 4
 #define COLONNE 4
+#define SEED 42
 
 int main() {
-	srand(time(NULL));
+    SDL_DisplayMode disp;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    initGraphics();
+    SDL_GetCurrentDisplayMode(0, &disp);
+    window = createWindow(500, 10, WINDOW_W, WINDOW_H);
+    renderer = createRenderer(window);
+    srand(SEED);
 	labyrinthe_t * labyrinthe = creerLabyrintheQqc(LIGNE, COLONNE,0.5);
 	generateGraphvizGraph(labyrinthe->graphe, "graphe_FYP");
 	liberer_graphe(labyrinthe->graphe);
